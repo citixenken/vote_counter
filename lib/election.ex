9205cb4c4370900b7680420be499a30f6a4b1421
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2024-01-11 21:40:26 +0300

    ability to vote

--- lib/election.ex
+++ lib/election.ex
@@ -53,6 +53,10 @@ defmodule Election do
     ]
   end
 
+  def update(election, cmd) when is_binary(cmd) do
+    update(election, String.split(cmd))
+  end
+
   def update(election, ["n" <> _ | args]) do
     name = Enum.join(args, " ")
     Map.put(election, :name, name)
@@ -70,6 +74,27 @@ defmodule Election do
     # OR
 
     %{election | candidates: candidates, next_id: election.next_id + 1}
+  end
+
+  def update(election, ["v" <> _ , id]) do
+    vote(election, Integer.parse(id))
+  end
+
+  defp vote(election, {id, _}) do
+    candidates = Enum.map(election.candidates, &maybe_inc_vote(&1, id))
+    Map.put(election, :candidates, candidates)
+  end
+
+  defp vote(election, _errors), do: election
+
+  defp maybe_inc_vote(candidate, id) when is_integer(id) do
+    maybe_inc_vote(candidate, candidate.id == id)
+  end
+
+  defp maybe_inc_vote(candidate, _inc_vote = false), do: candidate
+
+  defp maybe_inc_vote(candidate, _inc_vote = true) do
+    Map.update!(candidate, :votes, &(&1 + 1))
   end
 
   def view(election) do
