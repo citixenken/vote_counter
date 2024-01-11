1b9bc8cab6a8d24befbf3756b2d65a1917d4426c
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2024-01-11 12:49:26 +0300

    body + footer implementation

--- lib/election.ex
+++ lib/election.ex
@@ -18,4 +18,46 @@ defmodule Election do
   def view_header(election) do
     "Election for: #{election.name}\n"
   end
+
+  def view_body(election) do
+    election.candidates
+    |> sort_candidates_by_votes_desc()
+    |> candidates_to_strings()
+    |> prepend_candidates_header()
+  end
+
+  defp sort_candidates_by_votes_desc(candidates) do
+    candidates
+    |> Enum.sort(&(&1.votes >= &2.votes))
+  end
+
+  defp candidates_to_strings(candidates) do
+    candidates
+    |> Enum.map(fn %{id: id, name: name, votes: votes} ->
+      "#{id}\t#{votes}\t#{name}\n"
+    end)
+  end
+
+  defp prepend_candidates_header(candidates) do
+    [
+      "ID\tVotes\tName\n",
+      "-------------------------\n"
+      | candidates
+    ]
+  end
+
+  def view_footer() do
+    [
+      "\n",
+      "commands: (n)ame <election>, (a)dd <candidate>, (v)ote <id>, (q)uit\n"
+    ]
+  end
+
+  def view(election) do
+    [
+      view_header(election),
+      view_body(election),
+      view_footer()
+    ]
+  end
 end
